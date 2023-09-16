eaa95c5104040eba71c98f1b2d3eb0f8cfce13a1
Jchil7 <122590350+Jchil7@users.noreply.github.com>
2023-09-15 21:57:41 -0700

    Add files via upload

--- data_sftp_2.py
+++ data_sftp_2.py
@@ -1,7 +1,5 @@
 import paramiko
 
-#command = "df"
-
 # Update the next three lines with your
 # server's information
 
@@ -12,9 +10,12 @@ password = "Jbcasf18"
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
-#_stdin, _stdout,_stderr = client.exec_command("df")
+print("Connection made.")
+
+localpath = "/home/pi/sample.txt"
+remotepath = "/home/hank/sample.txt"
 sftp = client.open_sftp()
-sftp.put("pi/home/josh/'Hello World!'", "home/hank/Downloads/'Hello World!'")
+sftp.put(localpath, remotepath, callback=None, confirm=True)
 sftp.close()
 print("File transfer complete")
 client.close()
