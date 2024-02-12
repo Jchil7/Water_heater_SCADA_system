936eed0623b7f87f4e5b94a9df55b40193765fd9
Jchil7 <122590350+Jchil7@users.noreply.github.com>
2024-02-12 14:12:21 -0800

    Update Water_heater_data_tr.py

--- Water_heater_data_tr.py
+++ Water_heater_data_tr.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run("python log_data_sftp.py", shell=True)
-subprocess.run("python data_sftp_2_working.py", shell=True)
+subprocess.run("python water_draw_program.py", shell=True)
+subprocess.run("python data_sftp_2.py", shell=True)
