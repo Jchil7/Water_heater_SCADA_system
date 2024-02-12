84aa1445701fac745a4b8fd5e945d8bdadf71aa3
Jchil7 <122590350+Jchil7@users.noreply.github.com>
2024-02-12 14:25:20 -0800

    Update energy_usage_data.py

--- energy_usage_data.py
+++ energy_usage_data.py
@@ -1,8 +1,5 @@
-fname = "/home/pi/josh/CTA_EWH/build/debug/log.csv"
-
 # Function to read
 # last N lines of the file
-N = 1
 
 def LastNlines(fname, N):
     with open(fname, 'rt') as file:
@@ -10,3 +7,7 @@ def LastNlines(fname, N):
         # last n lines and print it
         for line in (file.readlines()[-N:]):
             print(line, end='')
+
+fname = "/home/pi/josh/CTA_EWH/build/debug/log.csv"
+N = 1
+LastNlines(fname, N)
